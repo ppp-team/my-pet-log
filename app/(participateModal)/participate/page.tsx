dbd79024c12036ca2998cb713732e114f3c812fb
Hayden <hayden.dev.k@gmail.com>
2024-01-27 02:45:33 +0900

    [#16] feat : participate 경로 직접 접근 시 redirect

--- app/(participateModal)/participate/page.tsx
+++ app/(participateModal)/participate/page.tsx
@@ -1,9 +1,12 @@
+/**
+ * url로 직접 접근 시 루트 경로로 리다이렉션
+ */
+
 import { NextPage } from "next";
-import "@/app/globals.css";
-import * as styles from "./page.css";
+import { redirect } from "next/navigation";
 
 const ParticipatePetGroupPage: NextPage = () => {
-  return <main className={styles.container}>펫 그룹 참여 모달 페이지</main>;
+  redirect("/");
 };
 
 export default ParticipatePetGroupPage;
