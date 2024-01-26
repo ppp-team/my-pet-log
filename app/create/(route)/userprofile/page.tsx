68527731981ed55a082abba5135a8134e8fac96d
Hayden <hayden.dev.k@gmail.com>
2024-01-27 00:25:01 +0900

    [#16] feat : 유저 프로필 페이지 추가

---
+++ app/create/(route)/userprofile/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { NextPage } from "next";
+import "@/app/globals.css";
+import * as styles from "./page.css";
+
+const CreateUserProfilePage: NextPage = () => {
+  return <section className={styles.container}>유저 프로필 생성</section>;
+};
+export default CreateUserProfilePage;
