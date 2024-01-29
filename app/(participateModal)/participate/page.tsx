40ae35d80159835246c9977e416714b95f952cf5
Hayden <hayden.dev.k@gmail.com>
2024-01-30 00:07:21 +0900

    [#32] feat : 펫그룹 참여 폼 구현

--- app/(participateModal)/participate/page.tsx
+++ app/(participateModal)/participate/page.tsx
@@ -2,11 +2,13 @@
  * url로 직접 접근 시 루트 경로로 리다이렉션
  */
 
+import ParticipatePetGroupModal from "@/components/ParticipatePetGroupModal/ParticipatePetGroupModal";
 import { NextPage } from "next";
 import { redirect } from "next/navigation";
 
 const ParticipatePetGroupPage: NextPage = () => {
-  redirect("/");
+  // redirect("/");
+  return <ParticipatePetGroupModal />;
 };
 
 export default ParticipatePetGroupPage;
--- components/ParticipatePetGroupModal/ParticipatePetGroupModal.tsx
+++ components/ParticipatePetGroupModal/ParticipatePetGroupModal.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from "next";
 import * as styles from "./ParticipatePetGroupModal.css";
+import Image from "next/image";
+import ParticipatePetGroupForm from "./ParticipatePetGroupForm";
 
 const ParticipatePetGroupModal = () => {
   return (
     <section className={styles.backDropContainer}>
       <section className={styles.modalContainer}>
         <p>전달 받으신 반려동물 등록 코드를 입력해주세요.</p>
+        <ParticipatePetGroupForm />
       </section>
     </section>
   );
