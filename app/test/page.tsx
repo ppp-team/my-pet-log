794a6284b5cefd155e6e6213dd2a4f00bde4c641
손지은 <wiseag@naver.com>
2024-01-30 22:47:24 +0900

    form controller test

---
+++ app/test/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Form, useForm } from "react-hook-form";
+import * as styles from "./style.css";
+
+const EditPage = () => {
+  const {
+    register,
+    control,
+    formState: { errors },
+    setError,
+    getValues,
+    watch,
+    clearErrors,
+  } = useForm({ mode: "onBlur" });
+
+  return (
+    <div className={styles.container}>
+      <Form
+        className={styles.form}
+        onSubmit={(data) => {
+          console.log(data);
+        }}
+        control={control}
+      >
+        <div className={styles.inputWrapper}>
+          <label htmlFor="title" className={styles.label}>
+            제목 *
+          </label>
+          <input
+            {...register("title", { required: "제목을 입력해주세요.", maxLength: { value: 15, message: "최대 15자까지 작성할 수 있습니다." } })}
+            maxLength={15}
+            id="title"
+            className={styles.input}
+          />
+          {watch("title") ? <p className={styles.p}>{watch("title")?.length}/ 15</p> : <p className={styles.p}>0/ 15</p>}
+          {errors["title"] && <p className={styles.error}>{errors["title"].message?.toString()}</p>}
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <label htmlFor="content" className={styles.label}>
+            내용
+          </label>
+          <input
+            {...register("content", { required: "내용을 입력해주세요.", maxLength: { value: 15, message: "최대 15자까지 작성할 수 있습니다." } })}
+            maxLength={15}
+            id="content"
+            className={styles.input}
+          />
+          {errors["content"] && <p className={styles.error}>{errors["content"].message?.toString()}</p>}
+        </div>
+        <button className={styles.button}>작성하기</button>
+      </Form>
+    </div>
+  );
+};
+
+export default EditPage;
