4cf39a5a15118d00a5b9420cf1025fbead901779
HyeyoonS <foryou9097@gmail.com>
2024-01-31 21:51:06 +0900

    [#56] feat : 비밀번호 타입지정

--- components/Sign/SignInput/PasswordInput.tsx
+++ components/Sign/SignInput/PasswordInput.tsx
@@ -5,7 +5,7 @@ import { inputContainer } from "./styles.css";
 const PasswordInput = forwardRef<HTMLInputElement, InputProps>(({ label, value, hasError, errorText, onChange, onBlur, placeholder }, ref) => {
   return (
     <div className={inputContainer}>
-      <Input ref={ref} label={label} value={value} hasError={hasError} errorText={errorText} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
+      <Input ref={ref} type="password" label={label} value={value} hasError={hasError} errorText={errorText} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
     </div>
   );
 });
