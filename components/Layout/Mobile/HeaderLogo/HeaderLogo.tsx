d6f81d8e06861bf647a876b7359c4d7760c6511d
Hayden <hayden.dev.k@gmail.com>
2024-01-31 20:16:18 +0900

    [#51] feat : HeaderLogo 임시 이미지 추가

--- components/Layout/Mobile/HeaderLogo/HeaderLogo.tsx
+++ components/Layout/Mobile/HeaderLogo/HeaderLogo.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import * as styles from "./HeaderLogo.css";
+import EditIconUrl from "@/assets/edit.svg?url";
 
 const HeaderLogo = () => {
-  return <header className={styles.header}>HeaderLogo</header>;
+  return (
+    <header className={styles.header}>
+      <Image src={EditIconUrl} alt="펫그룹 이미지" width={40} height={40} />
+    </header>
+  );
 };
 
 export default HeaderLogo;
