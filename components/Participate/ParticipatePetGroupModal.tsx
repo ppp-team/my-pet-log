6c934ce541976d6f63d28343238ae9aa53ef8cc2
Hayden <hayden.dev.k@gmail.com>
2024-01-31 21:16:01 +0900

    [#51] fix : Image 태그 width height 추가

--- components/Participate/ParticipatePetGroupModal.tsx
+++ components/Participate/ParticipatePetGroupModal.tsx
@@ -12,7 +12,7 @@ const ParticipatePetGroupModal = ({ onClickClose }: ParticipatePetGroupModalProp
   return (
     <section className={styles.modalContainer}>
       <button onClick={onClickClose}>
-        <Image src={sampleImageSrc} alt="모달 종료 버튼 이미지" />
+        <Image src={sampleImageSrc} alt="모달 종료 버튼 이미지" width={30} height={30} />
       </button>
       <p>전달 받으신 반려동물 등록 코드를 입력해주세요.</p>
       <ParticipatePetGroupForm />
