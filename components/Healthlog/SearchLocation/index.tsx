a41a7a599328d1750af28b5090323d8d1b572717
Jyophie <jyophie@gmail.com>
2024-01-31 16:20:22 +0900

    [#47] feat : 맵 API 검색 입력 연동

--- components/Healthlog/SearchLocation/index.tsx
+++ components/Healthlog/SearchLocation/index.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 declare global {
   interface Window {
     kakao: any;
   }
-  const kakao: any;
 }
 
 interface SearchLocationProps {
@@ -20,6 +19,9 @@ interface Place {
 type Status = "OK" | "ZERO_RESULT" | "ERROR";
 
 const SearchLocation = ({ appKey }: SearchLocationProps) => {
+  const [inputText, setInputText] = useState<string>("");
+  const [selectedPlace, setSelectedPlace] = useState<string>("");
+
   useEffect(() => {
     // 카카오 맵 스크립트 로딩
     const script = document.createElement("script");
@@ -28,27 +30,13 @@ const SearchLocation = ({ appKey }: SearchLocationProps) => {
     document.head.appendChild(script);
 
     script.onload = () => {
-      // 지도 생성
       kakao.maps.load(() => {
         const container = document.getElementById("map");
         const options = {
           center: new kakao.maps.LatLng(37.566826, 126.9786567),
           level: 3,
         };
-        const map = new kakao.maps.Map(container, options);
-
-        // 장소 검색 로직
-        const ps = new kakao.maps.services.Places();
-        ps.keywordSearch("이태원 맛집", (data: Place[], status: Status) => {
-          if (status === kakao.maps.services.Status.OK) {
-            const bounds = new kakao.maps.LatLngBounds();
-            for (let i = 0; i < data.length; i++) {
-              displayMarker(data[i], map);
-              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-            }
-            map.setBounds(bounds);
-          }
-        });
+        new kakao.maps.Map(container, options);
       });
     };
 
@@ -57,21 +45,48 @@ const SearchLocation = ({ appKey }: SearchLocationProps) => {
     };
   }, [appKey]);
 
-  // 마커 표시 함수
+  const searchPlaces = () => {
+    kakao.maps.load(() => {
+      const mapContainer = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3,
+      };
+      const map = new kakao.maps.Map(mapContainer, options);
+      const ps = new kakao.maps.services.Places();
+
+      ps.keywordSearch(inputText, (data: Place[], status: Status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const bounds = new kakao.maps.LatLngBounds();
+          for (let i = 0; i < data.length; i++) {
+            displayMarker(data[i], map);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }
+          map.setBounds(bounds);
+        }
+      });
+    });
+  };
+
   const displayMarker = (place: Place, map: any) => {
     const marker = new kakao.maps.Marker({
       map: map,
       position: new kakao.maps.LatLng(place.y, place.x),
     });
 
-    const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
     kakao.maps.event.addListener(marker, "click", () => {
-      infowindow.setContent(`<div style="padding:5px;font-size:12px;">${place.place_name}</div>`);
-      infowindow.open(map, marker);
+      setSelectedPlace(place.place_name);
     });
   };
 
-  return <div id="map" style={{ width: "100%", height: "30rem" }} />;
+  return (
+    <div>
+      <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} placeholder="검색어를 입력하세요" />
+      <button onClick={searchPlaces}>검색</button>
+      <div id="map" style={{ width: "100%", height: "30rem" }} />
+      <input type="text" value={selectedPlace} readOnly />
+    </div>
+  );
 };
 
 export default SearchLocation;
