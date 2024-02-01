ad6b7aafd1f596d76b254e340d5845004a57ede7
Hayden <hayden.dev.k@gmail.com>
2024-02-01 20:46:41 +0900

    [#57] feat : 공백 입력 무효화, required 에러메세지

--- components/Participate/ParticipatePetGroupForm.tsx
+++ components/Participate/ParticipatePetGroupForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { FormEvent } from "react";
 import { ERROR_MESSAGE } from "@/constatnts/inputConstant";
 import Image from "next/image";
 import sampleImageSrc from "@/assets/edit.svg?url";
@@ -9,17 +10,42 @@ const removeSpaces = (str: string) => {
   return str.replace(/\s/g, "");
 };
 
+interface IForm {
+  invitedCode: string;
+  invalid: string;
+}
+
 const ParticipatePetGroupForm = () => {
-  const { register, control, handleSubmit, watch } = useForm({ mode: "onChange" });
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<IForm>({ mode: "onChange" });
+
+  const onSubmit = (data: any) => {
+    console.log("onSubmit", data);
+  };
 
   return (
-    <Form control={control}>
-      <input type="text" placeholder="반려동물 등록 코드 입력" maxLength={10} {...register("code")} />
-      <div>
-        <Image src={sampleImageSrc} alt="주의 아이콘 이미지" width={30} height={30} />
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="반려동물 등록 코드 입력"
+        {...register("invitedCode", {
+          required: ERROR_MESSAGE.invitedCodeRequired,
+          onChange: (e: FormEvent<HTMLInputElement>) => setValue("invitedCode", removeSpaces(e.currentTarget.value)),
+        })}
+      />
+      {errors?.invitedCode?.message && (
+        <div>
+          <Image src={sampleImageSrc} alt="주의 아이콘 이미지" width={30} height={30} />
+          <p>{errors.invitedCode.message}</p>
+        </div>
+      )}
+
       <button type="submit">등록</button>
-    </Form>
+    </form>
   );
 };
 export default ParticipatePetGroupForm;
