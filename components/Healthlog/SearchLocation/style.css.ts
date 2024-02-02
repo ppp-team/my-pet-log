d2f4cb4ab9d8fbd00ca15e3ba923fdd9abf4515f
Jyophie <jyophie@gmail.com>
2024-02-02 11:57:59 +0900

    [#61] refactor : 카카오맵 오류 수정 및 레이아웃 수정

--- components/Healthlog/SearchLocation/style.css.ts
+++ components/Healthlog/SearchLocation/style.css.ts
@@ -1,16 +1,25 @@
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
+  width: "100%",
+
   display: "flex",
   flexDirection: "column",
 
   gap: "0.5rem",
 });
 
+export const searchContainer = style({
+  display: "flex",
+  alignItems: "center",
+});
+
 export const inputWrapper = style({
   width: "100%",
   height: "3rem",
 
+  flex: 1,
+  marginRight: "0.5rem",
   padding: "1rem",
 
   borderRadius: "10px",
@@ -23,7 +32,7 @@ export const inputWrapper = style({
 });
 
 export const searchButton = style({
-  width: "10rem",
+  width: "8rem",
   height: "3.6rem",
 
   borderRadius: "50px",
