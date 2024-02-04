922742250d126a8af81f3d39a6ba5bcbcee3fa0b
olseul <seul9085@naver.com>
2024-02-04 16:44:26 +0900

    [#93] refactor : 제목 헤더 컴포넌트 사용

--- app/(settingsMenu)/(petmate)/layout.tsx
+++ app/(settingsMenu)/(petmate)/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import * as styles from "./layout.css";
 import CloseIcon from "@/assets/invite-petcard.svg?url"; //추후 수정
 import Image from "next/image";
+import TitleHeader from "@/components/@common/TitleHeader";
 
 export default function PetmateLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -13,12 +14,7 @@ export default function PetmateLayout({ children }: { children: React.ReactNode 
   return (
     <section>
       <nav>
-        <header className={styles.header}>
-          펫메이트 초대 및 그룹 관리
-          <div className={styles.closeIcon} onClick={() => router.push("/settings")}>
-            <Image src={CloseIcon} alt="close icon" width={25} height={25} />
-          </div>
-        </header>
+        <TitleHeader title="펫메이트 초대 및 그룹 관리" redirectPath="/settings" />
         <ul className={styles.navList}>
           <li className={`${pathname === "/member" ? styles.active : styles.noActive}`}>
             <Link href="/member">펫메이트 그룹 관리</Link>
