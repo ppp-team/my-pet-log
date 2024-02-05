7177c0e59369affcf41fea029bc56b50f11d214a
HyeyoonS <foryou9097@gmail.com>
2024-02-05 21:53:34 +0900

    [#103] feat : petOptions 타입 추가

--- public/data/petOptions.ts
+++ public/data/petOptions.ts
@@ -7,7 +7,19 @@ import rabbitData from "./rabbit-type.json";
 import ratData from "./rat-type.json";
 import reptileData from "./reptile-type.json";
 
-export const petOptions = {
+interface PetOptionsProps {
+  강아지: string[];
+  고양이: string[];
+  물고기: string[];
+  햄스터: string[];
+  새: string[];
+  토끼: string[];
+  파충류: string[];
+  곤충: string[];
+  기타: string[];
+}
+
+export const petOptions: PetOptionsProps = {
   강아지: dogData,
   고양이: catData,
   물고기: fishData,
