1283c2e385cd6b3bf3cffc9f86b1c05b41cff6b1
olseul <seul9085@naver.com>
2024-02-08 19:36:28 +0900

    [#135] refactor : 그룹관리 컴포넌트 분리, 모달 색상 수정

---
+++ app/settings/_components/MemberList/index.tsx
@@ -0,0 +1,51 @@
+import NoProfileImage from "@/public/images/person-profile-default.svg?url";
+import { useModal } from "@/app/_hooks/useModal";
+import Modal from "@/app/_components/Modal";
+import { memberlist, profileWrapper, profileImg, nickname, button } from "@/app/settings/(petmate)/petmate.css";
+
+interface Member {
+  guardianId: string;
+  guardianRole: string;
+  nickname: string;
+  profileImageUrl: string | null;
+}
+
+interface MemberListProps {
+  members: Member[];
+  isLeader: boolean;
+}
+
+const MemberList = ({ members, isLeader }: MemberListProps) => {
+  const { isModalOpen, openModalFunc, closeModalFunc } = useModal();
+
+  // 확인 버튼 누를 시
+  const handleConfirm = () => {
+    closeModalFunc();
+  };
+
+  return (
+    <>
+      {members.map((member) => (
+        <section key={member.guardianId} className={memberlist}>
+          <div className={profileWrapper}>
+            <div
+              className={profileImg}
+              style={{
+                backgroundImage: `url(${member.profileImageUrl ?? NoProfileImage})`,
+              }}
+            />
+            <p className={nickname}>{member.nickname}</p>
+          </div>
+          {isLeader && (
+            <button className={button} onClick={openModalFunc}>
+              삭제
+            </button>
+          )}
+        </section>
+      ))}
+      {isModalOpen && <Modal text="정말 멤버를 삭제하시겠습니까?" buttonText="확인" onClick={handleConfirm} onClose={closeModalFunc} />}
+    </>
+  );
+};
+
+export default MemberList;
