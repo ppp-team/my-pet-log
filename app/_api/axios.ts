8aea8a8966548308b99137af843c09a8cf3553ea
손지은 <wiseag@naver.com>
2024-02-10 16:50:58 +0900

    Revert "[#146] feat : middleware.ts 설정"
    This reverts commit 2b29d5916e6a0c9889a9a1cf38bf5b343e421a69.

--- app/_api/axios.ts
+++ app/_api/axios.ts
@@ -1,20 +1,11 @@
 import axios from "axios";
-import { cookies } from "next/headers";
-
-const BASE_URL = "http://13.124.44.0:8001/api/v1";
+const BASE_URL = "url";
 
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-instance.interceptors.request.use((config) => {
-  const accessToken = cookies().get("accessToken")?.value;
-  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
-
-  return config;
 });
 
 export default instance;
