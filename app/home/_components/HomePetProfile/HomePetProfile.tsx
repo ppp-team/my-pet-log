1e3167e0a778fb47ff089679492b780580e8990f
Hayden <hayden.dev.k@gmail.com>
2024-02-14 18:59:22 +0900

    [#154] feat : 홈 펫 프로필 api 연동

--- app/home/_components/HomePetProfile/HomePetProfile.tsx
+++ app/home/_components/HomePetProfile/HomePetProfile.tsx
@@ -1,36 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import * as styles from "./HomePetProfile.css";
-import { PetGroupType } from "@/app/_types/petGroup";
+import { PetsType } from "@/app/_types/petGroup/pets";
 import calculateAge from "@/app/_utils/calculateAge";
 import NoPetProfileIconSrc from "@/public/images/pet-profile-default.svg?url";
-
-export const SAMPLE_CURRENT_PET: PetGroupType = {
-  petId: "3",
-  ownerId: "3",
-  inviteCode: "",
-  name: "멍멍이",
-  type: "",
-  breed: "말티즈",
-  gender: "여",
-  isNeutered: "Y",
-  birth: "2022.01.01",
-  weight: "5.5",
-  registNumber: "",
-  repStatus: "Y",
-  petImageUrl: null,
-};
+import { useQuery } from "@tanstack/react-query";
+import { getPets } from "@/app/_api/pets";
 
 const HomePetProfile = () => {
+  const { data: pets } = useQuery<PetsType>({
+    queryKey: ["pets"],
+    queryFn: () => getPets(),
+  });
+
+  const petList = pets?.data ?? [];
+
+  const currentPetGroup = petList.find((petGroup) => petGroup.repStatus === "REPRESENTATIVE") ?? petList[0];
+
+  if (!currentPetGroup) return <></>;
   return (
     <div className={styles.container}>
-      <Image className={styles.petProfileImage} src={SAMPLE_CURRENT_PET.petImageUrl ?? NoPetProfileIconSrc} alt="동물 프로필 이미지" width={180} height={180} priority={true} />
-      <p className={styles.petName}>{SAMPLE_CURRENT_PET.name}</p>
+      <Image className={styles.petProfileImage} src={currentPetGroup.petImageUrl ?? NoPetProfileIconSrc} alt="동물 프로필 이미지" width={180} height={180} priority={true} />
+      <p className={styles.petName}>{currentPetGroup.name}</p>
       <span className={styles.petInfo}>
-        {SAMPLE_CURRENT_PET?.breed && SAMPLE_CURRENT_PET.breed}
-        {SAMPLE_CURRENT_PET?.gender && " · " + SAMPLE_CURRENT_PET.gender}
-        {SAMPLE_CURRENT_PET?.birth && " · " + calculateAge(SAMPLE_CURRENT_PET.birth)}
+        {currentPetGroup.breed && currentPetGroup.breed}
+        {currentPetGroup.gender && " · " + (currentPetGroup.gender === "FEMALE" ? "여" : "남")}
+        {currentPetGroup.birth && " · " + calculateAge(currentPetGroup.birth)}
       </span>
-      <p className={styles.subtitle}>⭐️ {SAMPLE_CURRENT_PET.name}의 스케줄을 잊지 마세요!</p>
+      <p className={styles.subtitle}>⭐️ {currentPetGroup.name}의 스케줄을 잊지 마세요!</p>
     </div>
   );
 };
