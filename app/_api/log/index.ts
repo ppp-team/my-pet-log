3584c2886893013ef3ca16d5bbe611cc3f6781e9
Jyophie <jyophie@gmail.com>
2024-02-15 18:33:45 +0900

    [#133] feat : 로그 삭제 api 연동

--- app/_api/log/index.ts
+++ app/_api/log/index.ts
@@ -7,7 +7,9 @@ export const getLogs = async (petId: number, year: number, month: number, day: n
     const response = await instance.get(`/pets/${petId}/logs`, {
       params: { year, month, day },
     });
-    return response.data;
+    if (response.status === 200) {
+      return response.data;
+    }
   } catch (error: any) {
     console.error(error.response ? error.response.data : error.message);
     return [];
@@ -17,8 +19,22 @@ export const getLogs = async (petId: number, year: number, month: number, day: n
 export const getLogDetail = async (petId: number, logId: number) => {
   try {
     const response = await instance.get(`/pets/${petId}/logs/${logId}`, {});
-    return response.data;
+    if (response.status === 200) {
+      return response.data;
+    }
   } catch (error: any) {
     console.error(error.response ? error.response.data : error.message);
+  }
+};
+
+export const deleteLog = async (petId: number, logId: number) => {
+  try {
+    const response = await instance.delete(`/pets/${petId}/logs/${logId}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error: any) {
+    console.error(error.response.data);
+    return null;
   }
 };
--- app/_components/LogList/index.tsx
+++ app/_components/LogList/index.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import { deleteLog } from "@/app/_api/log";
 import LogItem from "@/app/_components/LogList/LogItem";
 import Modal from "@/app/_components/Modal";
 import { useModal } from "@/app/_hooks/useModal";
 import { LogListType, LogsType } from "@/app/_types/log/types";
 import EmptyHealthLog from "@/app/healthlog/_components/EmptyHealthLog";
-import React from "react";
+import { useMutation } from "@tanstack/react-query";
+import React, { useState } from "react";
 import * as styles from "./style.css";
 
-const LogList: React.FC<LogListType> = ({ logsData }) => {
-  const [selectedLog, setSelectedLog] = React.useState<LogsType | null>(null);
+const LogList: React.FC<LogListType> = ({ logsData: initialLogsData }) => {
+  const [selectedLog, setSelectedLog] = useState<LogsType | null>(null);
+  const [logsData, setLogsData] = useState(initialLogsData);
   const { isModalOpen, openModalFunc, closeModalFunc } = useModal();
+  const petId = 6;
+
+  const deleteLogMutation = useMutation({
+    mutationFn: (logId: number) => deleteLog(petId, logId),
+    onSuccess: () => {
+      setLogsData((prevLogsData) => ({
+        ...prevLogsData,
+        logs: prevLogsData.logs.filter((log) => log.logId !== selectedLog?.logId),
+      }));
+      closeModalFunc();
+    },
+  });
 
   const handleDelete = (logItem: LogsType) => {
     setSelectedLog(logItem);
@@ -18,10 +33,8 @@ const LogList: React.FC<LogListType> = ({ logsData }) => {
   };
 
   const confirmDelete = () => {
-    closeModalFunc();
     if (selectedLog) {
-      // 삭제 로직
-      console.log(`Deleting log with id: ${selectedLog.logId}`);
+      deleteLogMutation.mutate(selectedLog.logId);
     }
   };
 
