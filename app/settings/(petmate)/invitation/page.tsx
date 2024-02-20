3647ae8e242dae196bd82c5bffce7fc606ecbdd7
olseul <seul9085@naver.com>
2024-02-21 00:13:36 +0900

    [#193] feat : 초대코드, 펫정보 서버렌더링

--- app/settings/(petmate)/invitation/page.tsx
+++ app/settings/(petmate)/invitation/page.tsx
@@ -3,10 +3,13 @@
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 import Invitation from "@/app/settings/_components/Invitation";
 import { getMyInvitations } from "@/app/_api/invitation";
+import { getPet, getCode } from "@/app/_api/pets";
 
 const Page = async () => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({ queryKey: ["my-invitations"], queryFn: () => getMyInvitations() });
+  await queryClient.prefetchQuery({ queryKey: ["petInfo"], queryFn: () => getPet() });
+  await queryClient.prefetchQuery({ queryKey: ["inviteCode"], queryFn: () => getCode() });
 
   const dehydratedState = dehydrate(queryClient);
 
