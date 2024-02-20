ffcd019ce1ac7009a4df37f079b8d97ff7edfd03
Jyophie <jyophie@gmail.com>
2024-02-20 17:38:42 +0900

    [#180] refactor : HOME 로그 리스트 로직 수정

--- app/healthlog/page.css.ts
+++ app/healthlog/page.css.ts
@@ -15,38 +15,3 @@ export const title = style({
   fontSize: "1.6rem",
   fontWeight: "700",
 });
-
-export const calendarBox = style({
-  height: "30rem",
-
-  margin: "2.4rem 1.6rem",
-  padding: "1rem",
-
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-
-  border: "1px solid #d9d9d9",
-  borderRadius: "10px",
-  boxShadow: "0px 2px 4px 2px rgba(164, 164, 164, 0.25)",
-});
-
-export const calendar = style({
-  width: "100%",
-  height: "100%",
-});
-
-export const quickButtonsContainer = style({
-  whiteSpace: "nowrap",
-  overflowX: "auto",
-  margin: "1rem 0",
-
-  selectors: {
-    "&::-webkit-scrollbar": {
-      display: "none",
-    },
-  },
-
-  scrollbarWidth: "none",
-  msOverflowStyle: "none",
-});
--- app/home/_components/HomeHealthLogPreview/HomeHealthLogPreview.tsx
+++ app/home/_components/HomeHealthLogPreview/HomeHealthLogPreview.tsx
@@ -5,18 +5,24 @@ import LogList from "@/app/_components/LogList";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
-const HomeHealthLogPreview = () => {
+interface HomeHealthLogPreviewProps {
+  petId: number;
+}
+
+const HomeHealthLogPreview = ({ petId }: HomeHealthLogPreviewProps) => {
   const today = new Date().toISOString().split("T")[0];
   const [selectedDate, setSelectedDate] = useState(today);
   const [year, month, day] = selectedDate.split("-");
-  const petId = Number(localStorage.getItem("petId"));
 
   const { data: logsData } = useQuery({
     queryKey: ["Logs", petId, year, month, day],
     queryFn: () => getLogs(Number(petId), Number(year), Number(month), Number(day)),
-    enabled: !!petId,
   });
 
-  return <div>{logsData && <LogList logsData={logsData} petId={petId} />}</div>;
+  return (
+    <div>
+      <LogList petId={petId} selectedDate={selectedDate} logsData={logsData} />
+    </div>
+  );
 };
 export default HomeHealthLogPreview;
