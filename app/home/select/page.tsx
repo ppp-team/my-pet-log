e885e8d3d9fe8ab76e0eca6aa1b22fe0105f37be
Hayden <hayden.dev.k@gmail.com>
2024-02-20 11:20:08 +0900

    [#185] fix : HomeSelectPage 서버 컴포넌트로 변경

--- app/home/select/page.tsx
+++ app/home/select/page.tsx
@@ -1,9 +1,17 @@
-"use client";
-
+import { getPets } from "@/app/_api/pets";
 import SelectPet from "@/app/_components/SelectPet";
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 
-const HomeSelectPage = () => {
-  return <SelectPet type="기록" />;
+const HomeSelectPage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ["pets"], queryFn: () => getPets() });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <SelectPet type="기록" path="/home" />
+    </HydrationBoundary>
+  );
 };
 
 export default HomeSelectPage;
