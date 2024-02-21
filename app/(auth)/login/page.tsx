37975e73d011c0cbe754ad986b47394895e01516
Hyeyoon <foryou9097@gmail.com>
2024-02-21 10:57:54 +0900

    [#173] instance에 리프레시 토큰 설정중 (🐛bug)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/images/logo-high-resolution.png";
 import SignButton from "@/app/(auth)/_components/SignButton";
-import KakaoButton from "../_components/SignButton/KakaoButton";
+import KakaoButton from "@/app/(auth)/_components/SignButton/KakaoButton";
+import GoogleButton from "@/app/(auth)/_components/SignButton/GoogleButton";
 
 const Page = () => {
   return (
@@ -24,11 +25,9 @@ const Page = () => {
           <KakaoButton />
         </div>
 
-        {/* {session && (
-          <div className={styles.buttonWrapper}>
-            <SignButton type="google" action="로그인" onClick={() => signIn("google")} />
-          </div>
-        )} */}
+        <div className={styles.buttonWrapper}>
+          <GoogleButton />
+        </div>
 
         <Link className={styles.emailWrapper} href="/login/email">
           <SignButton type="email" action="로그인" />
--- app/_api/axios.ts
+++ app/_api/axios.ts
@@ -1,57 +1,151 @@
-"use server";
-import axios from "axios";
-import { cookies } from "next/headers";
+// import axios from "axios";
+// import { setCookie, getCookie } from "cookies-next";
+// import { cookies } from "next/headers";
 
-const BASE_URL = "http://13.124.44.0:8001/api/v1";
+// const baseURL = "http://13.124.44.0:8001/api/v1";
+
+// const instance = axios.create({
+//   baseURL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true,
+// });
+
+// instance.interceptors.request.use(
+//   async (config) => {
+//     const accessToken = getCookie("accessToken", { cookies });
+//     const refreshToken = getCookie("refreshToken", { cookies });
+
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+
+//     if (!accessToken || !refreshToken) {
+//       throw new Error("토큰이 없거나 만료되었습니다.");
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     console.error(error);
+//     return Promise.reject(error);
+//   },
+// );
+
+// instance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
+//     const refreshToken = getCookie("refreshToken", { cookies });
+
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+
+//       try {
+//         const { data } = await axios.post("/auth/refresh-token", { refreshToken });
+
+//         // 서버에서 받은 새로운 accessToken을 설정
+//         setCookie("accessToken", data.accessToken);
+
+//         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+//         return axios(originalRequest);
+//       } catch (error) {
+//         console.error(error);
+//         throw new Error("토큰을 확인해 주세요");
+//       }
+//     }
+
+//     return Promise.reject(error);
+//   },
+// );
+
+// export default instance;
+
+import axios from "axios";
+import { getCookie, setCookie } from "cookies-next";
 
 const instance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
+  baseURL: "http://13.124.44.0:8001/api/v1",
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    //쿠키에서 액세스토큰, 리프레시토큰 가져와서 헤더에 주입
+    const userAccessToken = getCookie(null, "accessToken");
+    const userRefreshToken = getCookie(null, "refreshToken");
+
+    if (userAccessToken) {
+      config.headers.Authorization = `Bearer ${userAccessToken}`;
+    }
+
+    if (userRefreshToken) {
+      config.headers["Authorization-refresh"] = userRefreshToken;
+    }
+
+    console.log("request start", config);
+    return config;
   },
-});
+  (error) => {
+    console.log("request error", error);
+    return Promise.reject(error);
+  },
+);
 
-instance.interceptors.request.use(async (config) => {
-  let accessToken = cookies().get("accessToken")?.value;
-  const refreshToken = cookies().get("refreshToken")?.value;
+instance.interceptors.response.use(
+  (response) => {
+    // 응답이 올바르다면 그대로 고
+    console.log("get response", response);
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+    if (status === 401) {
+      const originalRequest = config;
+      const userAccessToken = getCookie(null, "accessToken");
+      const userRefreshToken = getCookie(null, "refreshToken");
 
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
+      //리프레시토큰이 쿠키에 있는지 확인하고 리프레시토큰 있다면 NEW액세스토큰 가져와서 재요청
+      if (userAccessToken && userRefreshToken) {
+        try {
+          const { data } = await axios.post("auth/refresh-token", {
+            accessToken: userAccessToken,
+            refreshToken: userRefreshToken,
+          });
 
-  //토큰 만료여부 확인
-  const accessTokenExpiration = cookies().get("accessTokenExpiration");
-  //액세스토큰 없거나 만료된 경우인데
-  if (!accessToken || !accessTokenExpiration || new Date() > new Date(accessTokenExpiration)) {
-    //1. 리프레쉬토큰도 없는 경우: 에러던지고 로그인페이지로 이동해야하는데 이동안됨
-    if (!refreshToken) {
-      throw new Error("No refreshToken available.");
-    }
+          // NEW액세스토큰 쿠키에 저장
+          const { accessToken: newAccessToken } = data;
+          setCookie(null, "accessToken", newAccessToken);
 
-    //2. 리프레쉬토큰 있는경우 새로 토큰요청
-    try {
-      const res = await instance.post("/auth/refresh-token", { refreshToken });
+          //액세스토큰을 NEW액세스토큰으로 업데이트
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-      if (res.status === 200) {
-        accessToken = res.data.accessToken;
-
-        const accessTokenExpirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); //24시간
-
-        cookies().set("accessToken", accessToken, { expires: accessTokenExpirationDate });
-        cookies().set("accessTokenExpiration", accessTokenExpirationDate.toISOString(), { expires: accessTokenExpirationDate });
-        config.headers.Authorization = `Bearer ${accessToken}`;
-
-        //새로운 액세스토큰으로 요청보내기
-        return instance.request(config);
+          //재요청
+          return axios(originalRequest);
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      } else {
+        // 리프레시 토큰이 없으면 로그인 페이지로 이동
+        window.location.replace("/login");
       }
-    } catch (error) {
-      console.error(error);
-      //에러던지고 로그인페이지로 이동해야되는데 안됨
-      throw new Error("Error refreshing accessToken");
+      // 응답이 에러코드라면 로그인 페이지로 이동
+    } else if (status === 403) {
+      window.location.replace("/login");
+    } else if (status === 400) {
+      window.location.replace("/login");
     }
-  }
 
-  return config;
-});
+    console.log("response error", error);
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
