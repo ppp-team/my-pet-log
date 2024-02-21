dac10d883f621442310131157aeed363024b0efa
Hayden <hayden.dev.k@gmail.com>
2024-02-21 20:09:07 +0900

    [#203] fix : 메인 펫그룹 없는데 헤더(펫그룹) 페이지 접근시 404로 리다이렉션

--- app/_components/Layout/MobilePetGroupDropdown/MobilePetGroupDropdown.tsx
+++ app/_components/Layout/MobilePetGroupDropdown/MobilePetGroupDropdown.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import dropdownIconSrc from "@/public/icons/drop-down-icon-orange.svg?url";
 import petGroupSettingIconSrc from "@/public/icons/pet-group-settings.svg?url";
 import NoPetProfileIconSrc from "@/public/images/pet-profile-default.svg?url";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { PetsType } from "@/app/_types/petGroup/types";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { editPetRep, getPets } from "@/app/_api/pets";
@@ -45,6 +45,8 @@ const MobilePetGroupDropdown = () => {
    * @type {dropdownMenuItemType} currentPetGroupId로 리스트에서 repStatus === "REPRESENTATIVE"인 currentPetGroup 추출하고 없으면 null
    */
   const currentPetGroup: dropdownMenuItemType | null = parsedPetGroupList.find((petGroup) => petGroup.isSelected === true) ?? null;
+
+  if (!currentPetGroup) redirect("/404");
 
   /**
    * @type {Array<dropdownMenuItemType>} currentPetGroup 제외하고 나머지 리스트 + 동물 관리 버튼
