06ff07f5322359d8b855c98ae89a84cab6b3d9bc
Hayden <hayden.dev.k@gmail.com>
2024-02-21 21:21:46 +0900

    [#203] feat : Pc Header 및 글로벌 레이아웃 반응형 구현

--- app/_components/Layout/MainWrapper/MainWrapper.css.ts
+++ app/_components/Layout/MainWrapper/MainWrapper.css.ts
@@ -1,3 +1,13 @@
 import { style } from "@vanilla-extract/css";
 
-export const wrapper = style({ paddingTop: "5.6rem", paddingBottom: "6.3rem;" });
+export const wrapper = style({
+  paddingTop: "5.6rem",
+  paddingBottom: "6.3rem",
+  "@media": {
+    "screen and (min-width: 768px)": {},
+    "screen and (min-width: 1024px)": {
+      paddingTop: "8rem",
+      paddingBottom: "0",
+    },
+  },
+});
