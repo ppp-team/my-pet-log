78a095ceb1bd22371908285a99cd350c1115a7ea
손지은 <wiseag@naver.com>
2024-02-23 01:53:23 +0900

    [#211] refactor : 검색 추천 키워드 기능

--- app/diary/search/style.css.ts
+++ app/diary/search/style.css.ts
@@ -8,13 +8,41 @@ export const search = style({
   border: "none",
   background: "var(--GrayE8)",
 
-  padding: "0.8rem 1rem",
+  padding: "0.8rem 8rem 0.8rem 1.4rem",
+});
+
+export const deleteIcon = style({
+  width: "2rem",
+  height: "2rem",
+
+  color: "var(--Gray9f)",
 });
 
 export const searchIcon = style({
+  display: "flex",
+  gap: "1rem",
   position: "absolute",
 
-  right: "1rem",
+  right: 0,
   top: "50%",
   transform: "translate(-50%,-50%)",
+});
+
+export const searchIntroWrapper = style({
+  display: "flex",
+  flexDirection: "row",
+  gap: "2rem",
+
+  flexWrap: "wrap",
+
+  fontSize: "1.3rem",
+});
+
+export const recommendKey = style({
+  padding: "0.8rem 1.1rem",
+
+  borderRadius: "12px",
+  border: "1px solid var(--GrayE8)",
+
+  cursor: "pointer",
 });
