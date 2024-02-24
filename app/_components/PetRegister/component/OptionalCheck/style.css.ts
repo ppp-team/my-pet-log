c1bf9ec7bf71624ef960b759346a55e1ede8d51a
HyeyoonS <foryou9097@gmail.com>
2024-02-24 18:51:15 +0900

    [#225] design: optional-check 스타일링

--- app/_components/PetRegister/component/OptionalCheck/style.css.ts
+++ app/_components/PetRegister/component/OptionalCheck/style.css.ts
@@ -3,14 +3,17 @@ import { style } from "@vanilla-extract/css";
 export const container = style({
   display: "flex",
   alignItems: "center",
-  gap: "0.5rem",
 });
 
 export const itemWrapper = style({
+  marginTop: "0.6rem",
+  display: "flex",
+  alignItems: "center",
   cursor: "pointer",
 });
 
 export const checked = style({
+  marginLeft: "0.6rem",
   fontSize: "1.4rem",
   fontWeight: "500",
   color: "var(--Gray81)",
