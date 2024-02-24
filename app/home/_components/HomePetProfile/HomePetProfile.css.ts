19276439d321a7bf5d2bfb52c65fd34c781a5f56
Hayden <hayden.dev.k@gmail.com>
2024-02-24 19:23:56 +0900

    [#222] style : /home 태블릿 반응형 구현

--- app/home/_components/HomePetProfile/HomePetProfile.css.ts
+++ app/home/_components/HomePetProfile/HomePetProfile.css.ts
@@ -5,18 +5,57 @@ export const container = style({
   alignItems: "center",
   justifyItems: "center",
   marginBottom: "2.1rem",
+
+  "@media": {
+    "screen and (min-width: 744px)": {
+      marginBottom: "3.3rem",
+    },
+  },
 });
 
-export const petProfileImage = style({ width: "18rem", height: "18rem", marginBottom: "2rem", borderRadius: "50%", border: "5px solid var(--MainOrange)", objectFit: "cover" });
+export const petProfileImage = style({
+  width: "18rem",
+  height: "18rem",
+  marginBottom: "2rem",
+  borderRadius: "50%",
+  border: "5px solid var(--MainOrange)",
+  objectFit: "cover",
+
+  "@media": {
+    "screen and (min-width: 744px)": {
+      width: "25.4rem",
+      height: "25.4rem",
+      marginBottom: "2.4rem",
+    },
+  },
+});
 
 export const petName = style({
   marginBottom: "1rem",
   fontSize: "2rem",
   fontWeight: "700",
   color: "var(--Black)",
+
+  "@media": {
+    "screen and (min-width: 744px)": {
+      marginBottom: "1.2rem",
+      fontSize: "2.4rem",
+    },
+  },
 });
 
-export const petInfo = style({ marginBottom: "2rem", fontSize: "1.6rem", fontWeight: "500", color: "var(--Black)" });
+export const petInfo = style({
+  marginBottom: "2rem",
+  fontSize: "1.6rem",
+  fontWeight: "500",
+  color: "var(--Black)",
+
+  "@media": {
+    "screen and (min-width: 744px)": {
+      fontSize: "2rem",
+    },
+  },
+});
 
 export const subtitle = style({
   display: "inline-flex",
@@ -29,4 +68,11 @@ export const subtitle = style({
   fontSize: "1.4rem",
   fontWeight: "600",
   color: "var(--White)",
+
+  "@media": {
+    "screen and (min-width: 744px)": {
+      gap: "0.8rem",
+      fontSize: "1.6rem",
+    },
+  },
 });
