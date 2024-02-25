419aad4d74de8b46a7b2616c9dff3db0d363bd2c
olseul <seul9085@naver.com>
2024-02-25 17:05:02 +0900

    [#237] fix : 셋팅페이지 첫 렌더링 시 스켈레톤 ui안보임 이슈 수정

--- app/settings/_components/MypetCarousel/index.tsx
+++ app/settings/_components/MypetCarousel/index.tsx
@@ -3,9 +3,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+import "./swiper.css";
 import { Pagination } from "swiper/modules";
 import { petadd, container, petButton, petMateButton, petInfoWrapper } from "./style.css";
-import "./swiper.css";
 import MyPetInfo from "@/app/settings/_components/MyPetInfo";
 import Image from "next/image";
 import AddIcon from "@/public/icons/add.svg?url";
@@ -20,7 +20,11 @@ const MyPetCarousel = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
-  const { data: pets, isPending } = useQuery<PetsType>({
+  const {
+    data: pets,
+    isPending,
+    isSuccess,
+  } = useQuery<PetsType>({
     queryKey: ["pets"],
     queryFn: () => getPets(),
   });
@@ -39,53 +43,55 @@ const MyPetCarousel = () => {
     breedTextColor: "var(--White)",
   };
 
-  if (isPending) {
-    return <Skeleton />;
-  }
-
   return (
-    <div style={{ marginBottom: (pets?.data?.length ?? 0) > 0 ? "0" : "3.8rem" }}>
-      <Swiper
-        slidesPerView={"auto"}
-        centeredSlides={true}
-        spaceBetween={20}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {pets?.data.map((petInfo) => (
-          <SwiperSlide key={petInfo.petId}>
-            <div className={container}>
-              <div className={petInfoWrapper}>{petInfo && <MyPetInfo petInfo={petInfo} styles={myPetInfoStyles} />}</div>
-              <button
-                className={petButton}
-                onClick={() => {
-                  router.push(`/settings/pet-info/${petInfo.petId}`);
-                }}
-              >
-                마이펫 정보 수정
-              </button>
-              <button
-                onClick={() => {
-                  editPetRepMutate(String(petInfo.petId));
-                }}
-                className={petMateButton}
-              >
-                펫메이트 초대 및 그룹 관리
-              </button>
-            </div>
-          </SwiperSlide>
-        ))}
-        <SwiperSlide>
-          <Link href="/settings/pet-register" className={petadd}>
-            <Image src={AddIcon} alt="add icon" width={36} height={36} />
-            <span>마이펫 추가</span>
-          </Link>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <>
+      {isSuccess ? (
+        <div style={{ marginBottom: (pets?.data?.length ?? 0) > 0 ? "0" : "3.8rem" }}>
+          <Swiper
+            slidesPerView={"auto"}
+            centeredSlides={true}
+            spaceBetween={20}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {pets?.data.map((petInfo) => (
+              <SwiperSlide key={petInfo.petId}>
+                <div className={container}>
+                  <div className={petInfoWrapper}>{petInfo && <MyPetInfo petInfo={petInfo} styles={myPetInfoStyles} />}</div>
+                  <button
+                    className={petButton}
+                    onClick={() => {
+                      router.push(`/settings/pet-info/${petInfo.petId}`);
+                    }}
+                  >
+                    마이펫 정보 수정
+                  </button>
+                  <button
+                    onClick={() => {
+                      editPetRepMutate(String(petInfo.petId));
+                    }}
+                    className={petMateButton}
+                  >
+                    펫메이트 초대 및 그룹 관리
+                  </button>
+                </div>
+              </SwiperSlide>
+            ))}
+            <SwiperSlide>
+              <Link href="/settings/pet-register" className={petadd}>
+                <Image src={AddIcon} alt="add icon" width={36} height={36} />
+                <span>마이펫 추가</span>
+              </Link>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      ) : (
+        (isPending || isSuccess) && <Skeleton />
+      )}
+    </>
   );
 };
 
