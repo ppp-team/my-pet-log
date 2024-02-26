02b46ddcdeef25c876fc228a51d0eb396d79a846
HyeyoonS <foryou9097@gmail.com>
2024-02-26 16:27:39 +0900

    [#243] design : MainWrapperPcOnly 가운데 정렬 CSS속성 추가

--- app/_components/Layout/MainWrapperPcOnly/MainWrapperPcOnly.css.ts
+++ app/_components/Layout/MainWrapperPcOnly/MainWrapperPcOnly.css.ts
@@ -1,11 +1,15 @@
 import { style } from "@vanilla-extract/css";
 
 export const wrapper = style({
+  display: "flex",
+  justifyContent: "center",
   "@media": {
     "screen and (min-width: 744px)": {},
     "screen and (min-width: 1024px)": {
       paddingTop: "8rem",
       paddingBottom: "0",
+      // display: "flex",
+      // justifyContent: "center",
     },
   },
 });
