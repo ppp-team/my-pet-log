fcfebe49ee6e75fe672049518b1a785567885c23
손지은 <wiseag@naver.com>
2024-02-26 09:40:48 +0900

    [#240] feat : refresh-token 기능

--- app/_api/auth/index.ts
+++ app/_api/auth/index.ts
@@ -1,6 +1,7 @@
 "use server";
 import instance from "@/app/_api/axios";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 interface FormData {
   email: string;
@@ -16,6 +17,7 @@ export const postLogin = async ({ email, password }: FormData) => {
 
     if (res.status === 200) {
       cookies().set("accessToken", res.data.access_token);
+      cookies().set("refreshToken", res.headers.refreshtoken);
       return "signin success";
     }
   } catch (error: any) {
@@ -61,5 +63,33 @@ export const postSignup = async ({ email, password }: FormData) => {
   } catch (error: any) {
     console.error(error.response.data.message);
     return null;
+  }
+};
+
+export const getRefreshToken = async () => {
+  try {
+    const res = await fetch(`http://13.124.44.0:8001/api/v1/auth/refresh-token`, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies().get("accessToken")?.value}`,
+        refreshToken: `${cookies().get("refreshToken")?.value}`,
+      },
+    });
+    if (res.status == 200) {
+      //accessToken갱신
+      const data = await res.json();
+      cookies().set("accessToken", data.access_token);
+      return data.access_token;
+    } else {
+      throw new Error("refresh token expired");
+    }
+  } catch (e) {
+    //refresh-token도 만료됐다면 로그아웃
+    cookies().delete("accessToken");
+    cookies().delete("refreshToken");
+    cookies().delete("petId");
+    redirect("/login");
   }
 };
--- app/_api/axios.ts
+++ app/_api/axios.ts
@@ -1,5 +1,8 @@
+import { getRefreshToken } from "@/app/_api/auth";
 import axios from "axios";
+import ExpiryMap from "expiry-map";
 import { cookies } from "next/headers";
+import pMemoize from "p-memoize";
 
 const BASE_URL = "http://13.124.44.0:8001/api/v1";
 
@@ -17,4 +20,30 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
+const cache = new ExpiryMap(1000);
+const getRefreshTokenn = pMemoize(
+  async () => {
+    const newAccessToken = await getRefreshToken();
+    return newAccessToken;
+  },
+  { cache },
+);
+
+instance.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    //토큰 만료 에러
+
+    if (error.response.status === 401) {
+      const newAccessToken = await getRefreshTokenn();
+
+      if (newAccessToken) {
+        error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axios(error.config); //재요청
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 export default instance;
