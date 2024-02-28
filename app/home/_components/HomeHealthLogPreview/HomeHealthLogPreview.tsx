ea9b7c201fccde5652d63ec38637e10f3d2459a1
Hayden <hayden.dev.k@gmail.com>
2024-02-28 19:34:05 +0900

    [#253] feat : 홈 로딩 UI 추가

--- app/home/_components/HomeHealthLogPreview/HomeHealthLogPreview.tsx
+++ app/home/_components/HomeHealthLogPreview/HomeHealthLogPreview.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { getLogs } from "@/app/_api/log";
+import Loading from "@/app/_components/Loading";
 import LogList from "@/app/_components/LogList";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
@@ -14,15 +15,16 @@ const HomeHealthLogPreview = ({ petId }: HomeHealthLogPreviewProps) => {
   const [selectedDate, setSelectedDate] = useState(today);
   const [year, month, day] = selectedDate.split("-").map(Number);
 
-  const { data: logsData } = useQuery({
+  const { data: logsData, isPending } = useQuery({
     queryKey: ["Logs", petId, year, month, day],
     queryFn: () => getLogs(petId, year, month, day),
   });
 
   return (
-    <div>
+    <>
       <LogList petId={petId} selectedDate={selectedDate} logsData={logsData} />
-    </div>
+      {isPending && <Loading />}
+    </>
   );
 };
 export default HomeHealthLogPreview;
