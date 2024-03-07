45863f44e27a4d8a10ffdabe13cfe41b5d4d77bd
손지은 <wiseag@naver.com>
2024-03-08 00:57:23 +0900

    chore : deploy 설정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd home/ubuntu/ppp/my-pet-log
-git pull upstream main
+git pull upstream develop
 sudo npm install
+export NODE_OPTIONS=--max_old_space_size=800
 sudo npm run build
 pm2 restart my-pet-log
