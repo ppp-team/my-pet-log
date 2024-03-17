a77a25501707b286eb1883a18eb17a7dfd93267c
Hyeyoon <foryou9097@gmail.com>
2024-03-17 22:00:35 +0900

    [#288] feat : 구독자 관리페이지 영역별 폴더생성, 기초레이아웃

---
+++ app/settings/(pet-subscriber)/blocked-user/page.tsx
@@ -0,0 +1,22 @@
+"use server";
+
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+import Member from "@/app/settings/_components/Member";
+import { getMe } from "@/app/_api/users";
+import { getGuardians } from "@/app/_api/guardians";
+
+const Page = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: ["me"], queryFn: () => getMe() });
+  await queryClient.prefetchQuery({ queryKey: ["petmate"], queryFn: () => getGuardians() });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Member />
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
---
+++ app/settings/(pet-subscriber)/subscriber/page.tsx
@@ -0,0 +1,25 @@
+"use server";
+
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+import Invitation from "@/app/settings/_components/Invitation";
+import { getMyInvitations } from "@/app/_api/invitation";
+import { getPet, getCode } from "@/app/_api/pets";
+import { cookies } from "next/headers";
+
+const Page = async () => {
+  const queryClient = new QueryClient();
+  const petId = Number(cookies().get("petId")?.value);
+  await queryClient.prefetchQuery({ queryKey: ["my-invitations", petId], queryFn: () => getMyInvitations() });
+  await queryClient.prefetchQuery({ queryKey: ["petInfo", petId], queryFn: () => getPet(petId) });
+  await queryClient.prefetchQuery({ queryKey: ["inviteCode", petId], queryFn: () => getCode() });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Invitation petId={petId} />
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
